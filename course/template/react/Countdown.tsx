165b996fe8fb26f655c075406a40d56d051d6ee4
Fabiana Fonseca <fabiana.fonseca@vtex.com.br>
2020-07-01 14:29:17 -0300

    solution for step 8

--- course/template/react/Countdown.tsx
+++ course/template/react/Countdown.tsx
@@ -2,9 +2,12 @@ import React, { useState } from 'react'
 import { TimeSplit } from './typings/global'
 import { tick } from './utils/time'
 import { useCssHandles } from 'vtex.css-handles'
+import { useQuery } from 'react-apollo'
+import useProduct from 'vtex.product-context/useProduct'
+import productReleaseDate from './queries/productReleaseDate.graphql'
 
 interface CountdownProps {
-  targetDate: string
+
 }
 
 const DEFAULT_TARGET_DATE = new Date('2020-06-25').toISOString()
@@ -12,7 +15,7 @@ const DEFAULT_TARGET_DATE = new Date('2020-06-25').toISOString()
 const CSS_HANDLES = ['countdown']
 
 const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
-  targetDate = DEFAULT_TARGET_DATE,
+
 }) => {
   const [timeRemaining, setTime] = useState<TimeSplit>({
     hours: '00',
@@ -22,7 +25,34 @@ const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
 
   const handles = useCssHandles(CSS_HANDLES)
 
-  tick(targetDate, setTime)
+  const {
+    product: { linkText },
+  } = useProduct()
+  const { data, loading, error } = useQuery(productReleaseDate, {
+    variables: {
+      slug: linkText,
+    },
+    ssr: false,
+  })
+
+  if (loading) {
+    return (
+      <div>
+        <span>Loading...</span>
+      </div>
+    )
+  }
+  if (error) {
+    return (
+      <div>
+        <span>Error!</span>
+      </div>
+    )
+  }
+
+  tick(data?.product?.releaseDate || DEFAULT_TARGET_DATE, setTime)
+
+
 
   return (
     <div
